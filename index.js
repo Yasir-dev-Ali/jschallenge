92e813429a02ceddb3b03a5b1adc15eab209f9ae
Yasir Ali <91179875+yasirali786110@users.noreply.github.com>
2023-09-08 23:32:19 +0500

    add on task

--- index.js
+++ index.js
@@ -1,4 +1,34 @@
 
-// Wellcome To JavaScript Challenges
-// https://wellt.co/javascript-challenges
+console.log('Wellcome to JavaScript Challenge');
+// challenge start Here 
+// Write a program that outputs the phrase "Hello, world!" to the console.
+console.log("Hello, world");
 
+
+// 2> Write a Program to print this:
+// My Name is “Your Name”
+// I am Learning JavaScript via Full Stack Bootcamp of DiaaTech
+// I will become the best developer by hard work.
+// I am making commitment to do code with consistency.
+
+console.log(`My Name is Yasir Ali
+             I am Learning JavaScript via Full Stack Bootcamp of DiaaTech
+             I will become the best developer by hard work.
+             I am making commitment to do code with consistency.` 
+);
+
+
+
+// Variables
+//1 => Write a program that declares two variables, one with your name and
+// one with a number value of your age. Concatenate the two variables and
+// output the result to the console.
+
+let name ='Yasir ';
+let age=21;
+const fullName = ` Name was ${name} & Age  ${age}`; // concatenation operator
+console.log(fullName);
+
+
+// 2 =>  Repeat the coding task 2 of output by putting these strings in variables
+// and then outputting variables.
