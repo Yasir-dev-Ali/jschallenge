d702867b01504a7f63cb620309ff89b45b4bbecd
Yasir Ali <91179875+yasirali786110@users.noreply.github.com>
2023-09-10 22:07:42 +0500

    add task

--- index.js
+++ index.js
@@ -32,3 +32,47 @@ console.log(fullName);
 
 // 2 =>  Repeat the coding task 2 of output by putting these strings in variables
 // and then outputting variables.
+
+
+
+
+
+
+
+// 3=>. Initialize two variables and output their sum, minus, multiply and
+// division.
+var x =50 ; var y=49;
+console.log(`${x+y}`);
+console.log(`${x-y}`);
+console.log(`${x*y}`);
+console.log(`${x/y}`)
+
+
+
+// 4=>
+// BMI
+// Mark and John are trying to compare their BMI (Body Mass Index), which is calculated using
+// Task 
+// 1. Store Mark's and John's mass and height in variables
+
+let markMass = 70;
+let markHeight =  1.68;
+let johnMass = 90;
+let johnHeight = 1.95;
+
+//2. Calculate both their BMIs using the formula
+
+let  markBMI = markMass / (markHeight * markHeight);
+let johnBMI = johnMass / (johnHeight * johnHeight);
+
+console.log("Mark's BMI: " + markBMI);
+console.log("John's BMI: " + johnBMI);
+
+// 3. Create a Boolean variable 'markHigherBMI' containing information about whether Mark
+// has higher BMI than John. Print it out on the screen.
+if(markHeight>johnHeight){
+    console.log('Mark\'s Height Is Greater Than John');
+}else{
+    console.log('John\'s Height Is Greater Than Mark')
+}
+
