c243a432193293364e01ea9516a8a246c2256b78
Yasir Ali <91179875+yasirali786110@users.noreply.github.com>
2023-09-24 20:24:29 +0500

    add the loop task in this file

---
+++ Loop.js
@@ -0,0 +1,140 @@
+// Loop Challenges
+
+// 1. Write a program that uses a for loop to output the numbers from 1 to 10
+// to the console.
+
+
+// for(let i=1; i<=10; i++){
+//     console.log(i);
+// }
+
+// 2. Write a program that uses a for loop to output the even numbers from 0
+// to 20 to the console.
+
+// for(let i=0; i<=20; i++){
+//         console.log(i);
+//     }
+
+
+// 3. Write a program that uses a for loop to output the Fibonacci sequence
+// up to a 20 numbers.
+// let num1=0
+// let num2=1;
+// for(let num=1;num<=20;num++){
+//    let result=num1+num2;
+//    num1=num2;
+//    num2=result;
+//    console.log(result)
+
+// }
+
+
+// 4. Write a program that uses a for loop to output a multiplication table for
+// a certain number entered by the user. For example, if the user enters 5,
+// the program could output:
+
+// let  input = parseInt (prompt("Enter any number"));
+    // for(let  i=1; i<=10; i++){
+    //     console.log(`${input}x${i}= ${input*i}`);
+    //     };
+
+
+/*
+
+    Question 2
+    There are two gymnastics teams, Dolphins and Koalas.
+    
+    They compete against each other 3 times. The winner with the highest average
+    score wins a trophy!
+    Your Tasks
+    1. Calculate the average score for each team, using the test data below
+    2. Compare the team's average scores to determine the winner of the
+    competition, and print it to the console.
+    Don't forget that there can be a draw, so test for that as well (draw means they
+    have the same average score)
+    
+    Test data:
+    Data 1: Dolphins score 96, 108 and 89. Koalas score 88, 91 and 110
+    Data 2: Dolphins score 97, 112 and 101. Koalas score 109, 95 and 123
+    Data 3: Dolphins score 97, 112 and 101. Koalas score 109, 95 and 106
+    */
+   /*
+ Calculate the sum of the values
+const DolphinsSum = 96 + 108 + 89;
+console.log(`Dolphin Sum is : ${DolphinsSum}`)
+const KoalasSum =  88 + 91 + 110 ;
+console.log (`Koala Sum is:${KoalasSum}`);
+Calculate the Average Score
+const dolphinsAverageScore =( DolphinsSum/ 3);
+console.log('dolphins average score:' ,dolphinsAverageScore );
+const koalasAverageScore=(KoalasSum / 3 )
+console.log ('koalas average score:',koalasAverageScore);
+
+if(dolphinsAverageScore>koalasAverageScore){
+    console.log('dolphins Win');
+    
+}
+else{
+    console.log ("Koala win");
+    };
+
+*/
+
+
+
+
+
+// Data 2: Dolphins score 97, 112 and 101. Koalas score 109, 95 and 123
+
+/*
+let DolphinsScore= 97+112+101;
+let KoalasScore=   109+95+123;
+console.log(`Dolphin Sum is : ${DolphinsScore}`)
+console.log (`Koala Sum is:${KoalasScore}`);
+if(DolphinsScore>KoalasScore){
+    console.log("Dolphins Won")
+    }
+    else if((DolphinsScore==KoalasScore)){
+        console.log("Draw")}
+        else{
+            console.log("Koala won")
+        }
+        */
+
+
+
+// Data 3: Dolphins score 97, 112 and 101. Koalas score 109, 95 and 106
+/*
+let Dolphins_score= 97+112+101;
+let Koalas_score=    109+95+106;
+console.log(`Dolphin Sum is : ${Dolphins_score}`)
+console.log (`Koala Sum is:${Koalas_score}`);
+
+Calculate the Average Score
+const dolphinsAverageScore =( Dolphins_score/ 3);
+console.log('dolphins average score:' ,dolphinsAverageScore );
+const koalasAverageScore=(Koalas_score / 3 )
+console.log ('koalas average score:',koalasAverageScore);
+if (dolphinsAverageScore > koalasAverageScore) {
+    console.log('dolphins Win')
+    }
+    else{
+        console.log ('Koala wins')}
+    
+
+*/
+
+
+
+
+
+
+    
+   
+
+
+
+
+
+
+
